da89673181f69aa4a4746cf1485574446e9dabd2
Alexey Suprun <alexey.suprun.mail@gmail.com>
2024-06-26 20:29:13 +0200

    feat: create a model instance and parser to handle the base prompt

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -18,11 +18,34 @@ const getEntry = async (id: string) => {
 
 const EntryPage: FC<{ params: { id: string } }> = async ({ params }) => {
   const entry = await getEntry(params.id);
+  const analysisData = [
+    { name: 'Summary', value: '' },
+    { name: 'Subject', value: '' },
+    { name: 'Mood', value: '' },
+    { name: 'Negative', value: false },
+  ];
 
   return (
-    <div className="w-full h-full">
-      <Editor entry={entry} />
-      {params.id}
+    <div className="w-full h-full grid grid-cols-3">
+      <div className="col-span-2">
+        <Editor entry={entry} />
+      </div>
+      <div className="border-l border-black/10">
+        <div className="bg-blue-300 px-6 py-10">
+          <h2 className="text-2xl font-bold">Analysis</h2>
+        </div>
+        <ul>
+          {analysisData.map((item) => (
+            <li
+              key={item.name}
+              className="flex items-center justify-between py-3 px-3 border-b-2 border-black/10"
+            >
+              <h3 className="font-semibold">{item.name}</h3>
+              <span>{item.value.toString()}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
---
+++ utils/ai.ts
@@ -0,0 +1,34 @@
+import { ChatOpenAI } from '@langchain/openai';
+import { StructuredOutputParser } from 'langchain/output_parsers';
+import z from 'zod';
+
+const parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    mood: z.string().describe('the mood of the person who wrote the journal entry.'),
+    subject: z.string().describe('the subject of the journal entry.'),
+    negative: z
+      .boolean()
+      .describe('is the journal entry negative? (i.e. does it contain negative emotions?).'),
+    summary: z.string().describe('quick summary of the entire entry.'),
+    color: z
+      .string()
+      .describe(
+        'a hexidecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.',
+      ),
+    sentimentScore: z
+      .number()
+      .describe(
+        'sentiment of the text and rated on a scale from -10 to 10, where -10 is extremely negative, 0 is neutral, and 10 is extremely positive.',
+      ),
+  }),
+);
+
+export const analyze = async (prompt: any) => {
+  const model = new ChatOpenAI({
+    temperature: 0,
+    model: 'gpt-4o',
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const result = await model.invoke(prompt);
+  console.log(result);
+};
