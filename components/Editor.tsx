351a25d7d71a7a264a548d05836f9fafc0920234
Alexey Suprun <alexey.suprun.mail@gmail.com>
2024-07-11 00:18:59 +0200

    feat: create Alert ui-lib component and add it to Editor component

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -4,6 +4,7 @@ import { useCallback, useRef, useState } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
 import { FaRegTrashAlt } from 'react-icons/fa';
 import { useAutosave } from 'react-autosave';
+import { Alert } from '@/ui-lib';
 import { updateEntry, createNewEntry, deleteEntry } from '@/utils/api';
 
 const MINIMUM_CONTENT_LENGTH = 30;
@@ -16,6 +17,10 @@ const Editor = ({ entry }: { entry: { content: string; id?: string; analysis: An
   const [isLoading, setIsLoading] = useState(false);
   const [analysis, setAnalysis] = useState(entry?.analysis);
   const entryCreatedRef = useRef(false);
+
+  const isContentTooShort = contentValue.length < MINIMUM_CONTENT_LENGTH;
+  const isContentEntryCreated = entryCreatedRef.current;
+  const isCanBeDeleted = entry?.id;
 
   const saveContent = useCallback(
     async (_contentValue: string) => {
@@ -59,23 +64,32 @@ const Editor = ({ entry }: { entry: { content: string; id?: string; analysis: An
   ];
 
   return (
-    <div className="grid min-h-svh grid-cols-3 gap-10 pt-12">
-      <div className="relative col-span-2 pb-12 pl-10">
+    <div className="grid gap-10 pt-12 md:min-h-svh md:grid-cols-3">
+      <div className="relative px-5 pb-12 md:col-span-2 md:pl-10">
+        {isContentTooShort && (
+          <Alert>
+            Please enter at least 30 characters. Changes are not saved for entries with fewer than
+            30 characters.
+          </Alert>
+        )}
+        {isContentEntryCreated && (
+          <Alert type="success">A new note was created. Redirecting....</Alert>
+        )}
         {isLoading && <div className="loading loading-lg absolute inset-x-2/4 inset-y-2/4"></div>}
         <textarea
-          className="textarea h-full w-full resize-none p-10 text-xl outline-none"
+          className="textarea min-h-72 w-full resize-none p-10 text-xl outline-none md:min-h-svh"
           value={contentValue}
-          onChange={(e) =>
+          onChange={(e) => {
             setContentValue((prevValue: string) =>
               e.target.value !== '' ? e.target.value : prevValue,
-            )
-          }
+            );
+          }}
           placeholder="Please write your thoughts here..."
           maxLength={2500}
           required
         />
       </div>
-      <div className="border-l border-black/10 bg-gray-800 pr-10">
+      <div className="border-l border-black/10 bg-gray-800 md:pr-10">
         <div className="px-6 py-10" style={{ backgroundColor: color }}>
           <h2 className="w-fit bg-gray-800 p-6 text-2xl font-bold text-white">Analysis</h2>
         </div>
@@ -90,8 +104,8 @@ const Editor = ({ entry }: { entry: { content: string; id?: string; analysis: An
             </li>
           ))}
         </ul>
-        {entry?.id && (
-          <div className="mt-12 flex justify-end">
+        {isCanBeDeleted && (
+          <div className="mt-12 flex justify-end pb-10 pr-5 md:pb-0 md:pr-0">
             <button
               onClick={() => deleteEntryHandler(entry.id as string)}
               className="btn bg-red-800 text-white hover:bg-red-900"
---
+++ ui-lib/Alert.tsx
@@ -0,0 +1,43 @@
+function Alert({
+  children,
+  type,
+}: {
+  children: React.ReactNode;
+  type?: 'info' | 'warning' | 'error' | 'success';
+}) {
+  return (
+    <div role="alert" className={`alert ${type ? `alert-${type}` : ''} mb-5`}>
+      {type === 'success' ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 shrink-0 stroke-current"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 shrink-0 stroke-current"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+          />
+        </svg>
+      )}
+      <span>{children}</span>
+    </div>
+  );
+}
+export default Alert;
