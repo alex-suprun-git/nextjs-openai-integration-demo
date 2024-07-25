8cda591441e00fb92b6d7d936547278d30f79a50
Alexey Suprun <alexey.suprun.mail@gmail.com>
2024-07-25 19:09:28 +0200

    refactor: adjust header paddings on mobile

--- ui-lib/Header/index.tsx
+++ ui-lib/Header/index.tsx
@@ -1,7 +1,7 @@
 const Header = ({ children }: { children: React.ReactNode }) => (
   <div
     data-testid="header"
-    className="navbar border-black/10 bg-gray-900 px-10 py-5 md:items-center lg:h-[80px] lg:py-0"
+    className="navbar border-black/10 bg-gray-900 px-5 py-5 md:items-center md:px-10 lg:h-[80px] lg:py-0"
   >
     {children}
   </div>
