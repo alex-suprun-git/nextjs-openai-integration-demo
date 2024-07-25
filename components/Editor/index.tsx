d473a310438aaa8822e083ca8587c540456a3d4e
Alexey Suprun <alexey.suprun.mail@gmail.com>
2024-07-25 17:16:16 +0200

    feat: add a "return to journal" button for Editor component

--- components/Editor/index.tsx
+++ components/Editor/index.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+import { IoReturnUpBack } from 'react-icons/io5';
 import AnalysisSidebar from './AnalysisSidebar';
 import ContentField from './ContentField';
 import { useEditor } from '@/hooks/useEditor';
@@ -22,6 +24,12 @@ const Editor = ({ entry }: { entry: EditorEntry }) => {
   return (
     <div className="grid gap-10 pt-12 md:grid-cols-3 lg:min-h-svh">
       <div className="relative px-5 pb-12 text-stone-300 md:col-span-2 md:pl-10">
+        <Link href="/journal">
+          <div className="btn mb-8 border-0 bg-slate-900 text-stone-300 hover:bg-slate-900">
+            <IoReturnUpBack />
+          </div>
+        </Link>
+
         {isShowAutoSaveTimer && (
           <progress
             className="progress mb-2 w-full"
