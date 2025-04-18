f91ccc8044c67bc5c08102d28812338623895153
Alexey Suprun <alexey.suprun.mail@gmail.com>
2025-04-18 22:01:20 +0200

    test: verify  __session cookie manipulation to set custom domain key

--- apps/public/app/api/auth/status/route.ts
+++ apps/public/app/api/auth/status/route.ts
@@ -1,26 +1,12 @@
-import jwt from 'jsonwebtoken';
 import { NextRequest, NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 
-export async function GET(request: NextRequest) {
+export async function GET(_request: NextRequest) {
   const cookieStore = await cookies();
-  const publicKey = process.env.CLERK_PUBLISHABLE_KEY;
-
-  const token =
-    cookieStore.get('__session')?.value || request.headers.get('authorization')?.split(' ')[1];
-
+  const token = cookieStore.get('__client_uat')?.value;
   if (!token) {
     return NextResponse.json({ isSignedIn: false }, { status: 200 });
-  }
-
-  try {
-    const decoded = jwt.verify(token, publicKey!, {
-      algorithms: ['RS256'],
-    });
-
-    return NextResponse.json({ isSignedIn: true, userId: decoded.sub });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({ isSignedIn: false }, { status: 200 });
+  } else {
+    return NextResponse.json({ isSignedIn: true }, { status: 200 });
   }
 }
