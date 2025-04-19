8c7a3a4b0a46d14440dbd93d8d33533a0bcb8384
Alexey Suprun <alexey.suprun.mail@gmail.com>
2025-04-19 11:11:40 +0200

    feat: highlight current lang in the platform's lang switcher

--- apps/platform/components/LanguageSwitcher/index.tsx
+++ apps/platform/components/LanguageSwitcher/index.tsx
@@ -3,11 +3,14 @@
 import { MouseEvent } from 'react';
 import { GrLanguage } from 'react-icons/gr';
 import { setUserLocale } from '@/utils/locales';
+import { useLocale } from 'next-intl';
 
 const LanguageSwitcher = () => {
+  const locale = useLocale();
+
   const languages = [
-    { label: 'English', value: 'en' },
-    { label: 'Deutsch', value: 'de' },
+    { id: '01', label: 'English', value: 'en' },
+    { id: '02', label: 'Deutsch', value: 'de' },
   ];
 
   const handleLanguageChange = (e: MouseEvent<HTMLAnchorElement>, language: UserLocale) => {
@@ -32,17 +35,20 @@ const LanguageSwitcher = () => {
         tabIndex={0}
         className="dropdown-content menu z-[1] w-36 rounded-box bg-slate-800 p-2 shadow sm:w-40"
       >
-        {languages.map((language, index) => (
-          <li key={index}>
-            <a
-              href="#"
-              className="text-stone-300"
-              onClick={(e) => handleLanguageChange(e, language.value as UserLocale)}
-            >
-              {language.label}
-            </a>
-          </li>
-        ))}
+        {languages.map((language) => {
+          const isCurrentLang = language.value === locale;
+          return (
+            <li key={language.id}>
+              <a
+                href="#"
+                className="text-stone-300"
+                onClick={(e) => handleLanguageChange(e, language.value as UserLocale)}
+              >
+                {isCurrentLang ? <strong>{language.label}</strong> : language.label}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
