7ae1f52ca8ef8e8c3ae373f35fb8c4e1229fc9e3
Alexey Suprun <alexey.suprun.mail@gmail.com>
2026-01-19 08:31:35 +0100

    test(global-analytics): update test

--- packages/global-analytics/src/AnalyticsManager/index.test.tsx
+++ packages/global-analytics/src/AnalyticsManager/index.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import '@testing-library/jest-dom/vitest';
 import { act, render, waitFor } from '@testing-library/react';
 import { describe, it, expect, vi, beforeEach } from 'vitest';
 import AnalyticsManager from './index';
